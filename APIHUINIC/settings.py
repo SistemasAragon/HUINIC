25ebd135b8b99912b5e9afafbe68baba3f542a9c
SistemasAragon <sistemastec.aragon@gmail.com>
2024-08-08 11:39:58 -0600

    statis files

--- APIHUINIC/settings.py
+++ APIHUINIC/settings.py
@@ -120,12 +120,22 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+AUTH_USER_MODEL = 'api.Usuario'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+LOGIN_URL = '/login/'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+if not DEBUG:
+    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    #STATIC_ROOT = BASE_DIR / 'api/static'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'api', 'static')
+    
+    
+    
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
